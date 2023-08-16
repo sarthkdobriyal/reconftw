ec03c7cdef0a371ca92e3cfd9a81ad17fd40eb57
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2023-08-17 00:14:29 +0530

    cors settings and requirements update

--- web/web/settings.py
+++ web/web/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    os.path.join(BASE_DIR, 'dist', 'assets'),
+    os.path.join(BASE_DIR,'frontend' ,'dist', 'assets'),
 ]
 
 # STATIC_ROOT = BASE_DIR / "static"
@@ -211,10 +211,9 @@ CELERY_ROUTES = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:4173",
-    "http://localhost:8000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:4173",
-    "http://127.0.0.1:8000",
+    "http://3.110.51.70"
 ]
 
 
